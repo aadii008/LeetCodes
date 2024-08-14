4b89f4f5a8b5a648b81879f1209016e04a3a14e4
Aditya Gupta <147972180+aadii008@users.noreply.github.com>
2024-08-14 15:14:38 +0530

    Time: 2 ms (58.78%), Space: 7.4 MB (39.19%) - LeetHub

--- 1086-divisor-game/1086-divisor-game.cpp
+++ 1086-divisor-game/1086-divisor-game.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     bool divisorGame(int n) {
-        if(n % 2 == 0)
-        {
-            return true;
-        }
+        if(n%2==0) return true;
         return false;
     }
 };
