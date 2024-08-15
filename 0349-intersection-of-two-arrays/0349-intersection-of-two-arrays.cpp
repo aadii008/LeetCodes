09acdbe79113048f3d889d19edf31c07abe9d373
Aditya Gupta <147972180+aadii008@users.noreply.github.com>
2024-08-15 11:43:22 +0530

    Time: 6 ms (38.48%), Space: 14.6 MB (9.15%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -4,7 +4,7 @@ public:
        unordered_set<int> a(nums1.begin(), nums1.end());
        unordered_set<int> b(nums2.begin(), nums2.end());
         vector<int> ans;
-        for (auto num : a) {
+        for (const auto num : a) {
             if (b.find(num) != b.end()) ans.push_back(num);
         }
         return ans;
