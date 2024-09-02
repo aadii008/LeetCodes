ccd9e34e0b1ce66ccf01d20b53fd13c8365cb51a
Aditya Gupta <147972180+aadii008@users.noreply.github.com>
2024-09-03 01:39:02 +0530

    Time: 0 ms (100%), Space: 8.1 MB (60.18%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        int j = 0;
+        for (int i = 0; i < s.length(); i++){
+            if (isdigit(s[i]))
+                j--;
+            else
+                s[j++] = s[i];
+        }
+        return s.substr(0, j);
+    }
+};
