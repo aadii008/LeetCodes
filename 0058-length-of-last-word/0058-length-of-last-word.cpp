b371c73e4a560e5611e70e796f7bf930900430e6
Aditya Gupta <147972180+aadii008@users.noreply.github.com>
2024-09-04 17:59:15 +0530

    Time: 4 ms (47.08%), Space: 7.9 MB (61.59%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -5,12 +5,11 @@ public:
        int cnt=0;
        bool a= false;
        for(int i=n-1;i>=0;i--){
-        if(s[n-1])
         if(s[i]!=' '){
         cnt++;
         a=true;
         }
-        else if (a) break;
+        else if (a==true) break;
        } 
        return cnt;
     }
